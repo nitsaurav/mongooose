0f9f9215b8a0779eb17e9b3f06e4abc8f7e8ba80
sauravdevp <saurav.kumar@brightcodess.com>
2022-11-30 17:30:41 +0530

    add

--- olympicapi/mernbackend/src/app.js
+++ olympicapi/mernbackend/src/app.js
@@ -1,7 +1,13 @@
 const express=require("express");
+require("./db/conn");
 const app=express();
 const port = process.env.PORT || 3000;
+const path=require("path");
+const static_path=path.join(__dirname);
 
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+// console.log(path.join(__dirname));
 app.get('/', async(req,res)=>{
     res.send("hlo from india");
 })
